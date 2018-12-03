105f0e3e4b624610db391d847eec3b7a868233ef
Mahan Basu <30750347+Mohon111@users.noreply.github.com>
2018-12-03 16:34:20 +0600

    Update Lesson-3.py

--- Lesson-3.py
+++ Lesson-3.py
@@ -6,6 +6,7 @@
 Text1 = "Hi...! "
 Text2 = "I'm "
 Text3 = "Mohon Basu"
-Num = 18 + 8
+After2000, Before2000 = 18, 8
+Num = After2000 + Before2000
 ConNum = str(Num)
 print(Text1 + Text2 + Text3 + ".\n" + Text2 + ConNum)
