cdcb190e31686466aa21fec37ae07bf6da063eee
mahen <mahendrabashumohon@gmaail.com>
2018-12-04 14:58:47 +0600

    remote add origin https://github.com/Mohon111/Learning-Python.git

---
+++ Lesson-6.py
@@ -0,0 +1,9 @@
+#for loop & list[array]
+list = [23, 45, 33, 66, 34, 77, 99, 86, 74, 54, 65, 95]
+i = 0
+while (i <= 9):
+    print(list[i])
+    i+=1
+#Dynamic
+for x in list:
+    print(x)
---
+++ Lesson-7.py
@@ -0,0 +1,8 @@
+#function
+def test_function():
+    print("Say hello to world!")
+    a, b = 45, 65
+    add = a + b
+    print(add)
+test_function()
+test_function()
