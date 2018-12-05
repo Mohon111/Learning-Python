da6400f48227567d9390172354864452bc54d306
mahen <mahendrabashumohon@gmaail.com>
2018-12-05 17:21:20 +0600

    remote add origin https://github.com/Mohon111/Learning-Python.git

---
+++ Lesson-12.py
@@ -0,0 +1,11 @@
+#list & truple
+List = [43, 65, 85, 6, 53, 85, 2, 54]
+Truple = (43, 65, 85, 6, 53, 85, 2, 54)
+
+List[0] = 5
+#Truple[1] = 8
+print(List[0])
+print(Truple[1])
+
+print(List[-1])
+print(Truple[-1])
